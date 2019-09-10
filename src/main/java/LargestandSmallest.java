4b8896387c95a9286407766afbf78030640e67d1
0273898 <0273898@KELCE-LABS13-P.pittstate.edu>
2019-09-10 12:13:25 -0500

    initial commit

---
+++ src/main/java/LargestandSmallest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 0273898
+ */
+public class LargestandSmallest {
+    public static void main(String[] args) {
+      
+        int largestNumber = 0;
+        int smallestNumber = 0;
+        
+        int currentEntry = 0;
+        
+        while(currentEntry != 99) {
+            
+            currentEntry = 
+                    
+        }
+        }
+}
